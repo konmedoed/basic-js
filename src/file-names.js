9d3516dd7dc08ed9076688e9a9fd16efcda3ea92
Kirill Kostin <79797389+konmedoed@users.noreply.github.com>
2023-04-18 02:42:54 +0300

    feat: add a few done tasks (Tue, Apr 18, 2023 2:42:33)

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let arr = names.slice();
+  for (let i=0; i<arr.length; i++){      
+      let ind = 1;
+    for (let j=i+1; j<arr.length; j++){
+      if (arr[i]===arr[j]){
+          arr[j]=(arr[i]+`(${ind})`);
+          ind++;
+      }   
+    }
+  }
+  return arr
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let brr=arr.slice();
+    let skipArr=[];
+    brr.forEach((item, index)=>{
+        if (item===-1) skipArr.push(index)
+    })
+    brr = brr.filter(item=>item!==-1)
+    brr.sort((a,b)=>{
+        if (a<b) return -1
+        if (a>b) return 1;
+        return 0
+    })
+    skipArr.forEach(item=>{
+        brr.splice(item,0,-1)
+    })
+    return brr
 }
 
 module.exports = {
